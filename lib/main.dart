e65a805bdbf9251690192417ee0cc91ff090689b
marcelscruz <marcelcruz@live.com>
2020-03-22 23:47:54 -0300

    Add destination details

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Color(0xFF3EBACE),
         accentColor: Color(0xFFD8ECF1),
-        scaffoldBackgroundColor: Colors.grey[700],
-        // scaffoldBackgroundColor: Color(0xFFF3F5F7),
+        scaffoldBackgroundColor: Color(0xFFF3F5F7),
       ),
       home: HomeScreen(),
     );
